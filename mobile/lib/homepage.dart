ae038519279ffc7f42811fb196f93afa2e1ee7f6
anthonymeny <anthony.meny@noctua.care>
2024-01-30 11:26:27 +0100

    camera

--- mobile/lib/homepage.dart
+++ mobile/lib/homepage.dart
@@ -1,10 +1,37 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+import 'camera_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home Page")),
+      body: SafeArea(
+        child: Center(
+            child: ElevatedButton(
+          onPressed: () async {
+            List<CameraDescription> cameras = await availableCameras();
+            if (cameras.isNotEmpty) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => CameraPage(cameras: cameras)),
+              );
+            } else {
+              print("No cameras available.");
+            }
+          },
+          child: const Text("Take a Picture"),
+        )),
+      ),
+    );
   }
 }
